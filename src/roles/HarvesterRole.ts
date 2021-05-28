51c2909d57c18706a3efb81b1fcc2b06eaab5975
direded <dmpalyonniy@gmail.com>
2021-05-28 15:25:07 +0300

    Refactoring

--- src/roles/HarvesterRole.ts
+++ src/roles/HarvesterRole.ts
@@ -1,30 +1,30 @@
 export class HarvesterRole implements CreepRole {
 
-    public create(spawn: StructureSpawn): number {
-        return spawn.spawnCreep([WORK, CARRY, MOVE], "Harvester #" + Game.time, { memory: { role: "harvester" } })
-    }
+	public create(spawn: StructureSpawn): number {
+		return spawn.spawnCreep([WORK, CARRY, MOVE], "Harvester #" + Game.time, { memory: { role: "harvester" } })
+	}
 
-    public update(creep: Creep): void {
-        if (creep.store.getFreeCapacity() > 0) {
-            var sources = creep.room.find(FIND_SOURCES)
-            if (creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(sources[0], { visualizePathStyle: { stroke: '#ffaa00' } })
-            }
-        } else {
-            var targets = creep.room.find(FIND_STRUCTURES, {
-                filter: (structure) => {
-                    return (structure.structureType == STRUCTURE_EXTENSION ||
-                        structure.structureType == STRUCTURE_SPAWN ||
-                        structure.structureType == STRUCTURE_TOWER) &&
-                        structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0
-                }
-            })
-            if (targets.length > 0) {
-                if (creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(targets[0], { visualizePathStyle: { stroke: '#ffffff' } })
-                }
-            }
-        }
-    }
+	public update(creep: Creep): void {
+		if (creep.store.getFreeCapacity() > 0) {
+			var sources = creep.room.find(FIND_SOURCES)
+			if (creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+				creep.moveTo(sources[0], { visualizePathStyle: { stroke: '#ffaa00' } })
+			}
+		} else {
+			var targets = creep.room.find(FIND_STRUCTURES, {
+				filter: (structure) => {
+					return (structure.structureType == STRUCTURE_EXTENSION ||
+						structure.structureType == STRUCTURE_SPAWN ||
+						structure.structureType == STRUCTURE_TOWER) &&
+						structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0
+				}
+			})
+			if (targets.length > 0) {
+				if (creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+					creep.moveTo(targets[0], { visualizePathStyle: { stroke: '#ffffff' } })
+				}
+			}
+		}
+	}
 
 }
